2ae91ab2d9fe32c832f4b300ce8ad2e250079b56
Clément <clemos@horcruxe.(none)>
2011-11-30 10:49:04 +0100

    remove conflict data + small bugfix on autoimport (with no package line) + removed HaxeComplete.pyc

--- HaxeComplete.py
+++ HaxeComplete.py
@@ -69,8 +69,6 @@ class HaxeBuild :
 		return outp.strip()
 
 
-<<<<<<< HEAD
-=======
 class HaxeGenerateImport( sublime_plugin.TextCommand ):
 
 	start = None
@@ -140,77 +138,9 @@ class HaxeGenerateImport( sublime_plugin.TextCommand ):
 			if not pkg is None:
 				ins = "\n\nimport {0};".format(cname)
 				view.insert(edit, pkg.end(0), ins)
-
-
-class HaxeGenerateImport( sublime_plugin.TextCommand ):
-
-	start = None
-	size = None
-	cname = None
-
-	def get_end( self, src, offset ) :
-		end = len(src)
-		while offset < end:
-			c = src[offset]
-			offset += 1
-			if spaceChars.match(c): break
-		return offset - 1
-
-	def get_start( self, src, offset ) :
-		foundWord = 0;
-		while offset > 0:
-			c = src[offset]
-			offset -= 1
-			if foundWord == 0:
-				if spaceChars.match(c): continue
-				foundWord = 1
-			if not wordChars.match(c): break
-		return offset + 2
-	
-	def get_classname( self, view, src ) :
-		loc = view.sel()[0]
-		end = max(loc.a, loc.b)
-		self.size = loc.size()
-		if self.size == 0:
-			end = self.get_end(src, end)
-			self.start = self.get_start(src, end)
-			self.size = end - self.start
-		else:
-			self.start = end - self.size
-		self.cname = view.substr(sublime.Region(self.start, end)).rpartition(".")
-
-	def compact_classname( self, edit, view ) :
-		view.replace(edit, sublime.Region(self.start, self.start+self.size), self.cname[2])
-		view.sel().clear()
-		loc = self.start + len(self.cname[2])
-		view.sel().add(sublime.Region(loc, loc))
-
-	def get_indent( self, src, index ) :
-		print("first=" + src[index])
-		if src[index] == "\n": return index + 1
-		return index
-
-	def insert_import( self, edit, view, src) :
-		cname = "".join(self.cname)
-		clow = cname.lower()
-		last = None
-
-		for imp in importLine.finditer(src):
-			if clow < imp.group(2).lower():
-				print(imp.group(0))
-				ins = "{0}import {1};\n".format(imp.group(1), cname)
-				view.insert(edit, self.get_indent(src, imp.start(0)), ins)
-				return
-			last = imp
-
-		if not last is None:
-			ins = ";\n{0}import {1}".format(last.group(1), cname)
-			view.insert(edit, last.end(2), ins)
-		else:
-			pkg = packageLine.search(src)
-			if not pkg is None:
-				ins = "\n\nimport {0};".format(cname)
-				view.insert(edit, pkg.end(0), ins)
+			else:
+				ins = "import {0};\n\n".format(cname)
+				view.insert(edit, 0, ins)
 
 	def run( self , edit ) :
 		complete = HaxeComplete.inst
@@ -230,7 +160,6 @@ class HaxeGenerateImport( sublime_plugin.TextCommand ):
 		
 		self.insert_import(edit, view, src)		
 
->>>>>>> a7f7081a8401d3237415083f53f051b82614c742
 
 class HaxeRunBuild( sublime_plugin.TextCommand ):
 	def run( self , edit ) :
